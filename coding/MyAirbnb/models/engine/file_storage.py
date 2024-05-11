29b45451cb8466048acba5e72ae91bfd760e8482
O-keita <o.keita@alustudent.com>
2024-05-11 15:00:28 +0000

    still on the file storage

--- coding/MyAirbnb/models/engine/file_storage.py
+++ coding/MyAirbnb/models/engine/file_storage.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
 import json
-import models
-class FileStorage:
-    __file_path ='file.json'
-    __objects = {}
+from models.base_model import BaseModel
 
+class FileStorage():
+    """ serializes instances to a JSON file and deserializes JSON"""
+
+    __file_path = "file.json"  # path to the JSON file (ex: file.json)
+    __objects = {}  # dictionary - store all objects by <class name>.id
 
     def all(self):
+        """  returns the dictionary __objects """
         return self.__objects
-    
-    def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
 
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
+        '''get key of the form <obj class name>.id '''
+        key = obj.__class__.__name__ + "." + str(obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
 
-        serialize_obj = {}
+        ''' create empty dictionary'''
+        json_object = {}
+        """ fill dictionary with elements __objects """
+        for key in self.__objects:
+            json_object[key] = self.__objects[key].to_dict()
 
-
-        for key, obj in self.__objects.items():
-
-            serialize_obj[key] = obj.to_dict()
-
-        with open(self.__file_path, 'w') as file:
-
-            json.dump(serialize_obj, file)
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_object, f)
 
     def reload(self):
         """ deserializes the JSON file to __objects """
         try:
-            with open(self.__file_path, 'r', encoding="utf-8") as f:
+            with open(self.__file_path, 'r', encoding="UTF8") as f:
                 # jlo = json.load(f)
                 for key, value in json.load(f).items():
                     attri_value = eval(value["__class__"])(**value)
